31f18e3c7231e0dadad468db1c2dab014987fb67
harleymacedo <harley.ip@gmail.com>
2022-10-26 11:03:59 -0300

    Conversor funcionando para várias moedas

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -5,11 +5,20 @@ const Conversor = (props) => {
     const [valor, setValor] = useState('');
     const [resultado, setResultado] = useState('');
     const [cor, setCor] = useState(props.cor);
-    const [origin, setOrigem] = useState(props.origin);
+    const [origem, setOrigem] = useState(props.origem);
     const [destino, setDestino] = useState(props.destino);
 
     const atualizarValor = (event) => {
         setValor(event.target.value);
+    }
+
+    const converter = async () => {
+        let url = `https://economia.awesomeapi.com.br/json/last/${origem}-${destino}`;
+        console.log(url);
+        let result = await fetch(url);
+        let dados = await result.json();
+        console.log(dados);
+        setResultado( (parseFloat(valor) * dados[origem+destino].bid).toFixed(4) );
     }
 
     return (
@@ -21,6 +30,7 @@ const Conversor = (props) => {
         }}>
             <p>Converter de {props.origem} para {props.destino}</p>
             <input type='text' placeholder='Digite o valor' onChange={atualizarValor} />
+            <button onClick={converter} >Converter</button>
             <p>Resultado:</p>
             <p>{resultado}</p>
         </div>
